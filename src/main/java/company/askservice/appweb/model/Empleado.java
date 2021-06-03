62a72ae2f4f45dfc1cbf47ffd9079bcc4afd415b
DeamBrose <emiliodragon98@hotmail.com>
2021-06-02 22:32:56 -0500

    Arreglando

--- src/main/java/company/askservice/appweb/model/Empleado.java
+++ src/main/java/company/askservice/appweb/model/Empleado.java
@@ -15,13 +15,10 @@ public class Empleado implements Serializable{
     private Long id;
 
     @Column(length = 100)
-    private String nombre;
-
-    @Column(length = 100)
     private String apellido;
 
-    @Column(length = 9, nullable = false, unique = true)
-    private String telefono;
+    @Column(length = 500)
+    private String descripción;
 
     @Column(length = 8, nullable = false, unique = true)
     private String dni;
@@ -29,98 +26,16 @@ public class Empleado implements Serializable{
     @Column(length = 45, nullable = false, unique = true)
     private String email;
 
-    @Column(length = 500)
-    private String descripción;
-
     @Column(length = 5)
     private String estado;
+
+    @Column(length = 100)
+    private String nombre;
+
+    @Column(length = 9, nullable = false, unique = true)
+    private String telefono;
 
     @ManyToMany
     private List<Servicio> servicios;
 
-<<<<<<< HEAD
-=======
-    public Empleado() {
-    }
-
-    public Empleado(Long id, String nombre, String apellido, Integer telefono, Integer dni, String email, String descripción, String estado, List<Servicio> servicios) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.dni = dni;
-        this.email = email;
-        this.descripción = descripción;
-        this.estado = estado;
-        this.servicios = servicios;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public Integer getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(Integer telefono) {
-        this.telefono = telefono;
-    }
-
-    public Integer getDni() {
-        return dni;
-    }
-
-    public void setDni(Integer dni) {
-        this.dni = dni;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDescripción() {
-        return descripción;
-    }
-
-    public void setDescripción(String descripción) {
-        this.descripción = descripción;
-    }
-
-    public String getEstado() { return estado; }
-
-    public void setEstado(String estado) { this.estado = estado; }
-
-    public List<Servicio> getServicios() {
-        return servicios;
-    }
-
-    public void setServicios(List<Servicio> servicios) {
-        this.servicios = servicios;
-    }
->>>>>>> bb78ca506a36d9d643093bddd37d0079e35fd489
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 insert into cliente values (null,'Ortiz Diaz','SJM, IDAT, PLOP','natalyortiz.401@gmail.con','Melissa Nataly','923456123');
 
 insert into cliente values (null,'Torres Rosas','SJM, IDAT, PLOP','lalalalalal@gmail.con','Juan','923456555');
@@ -9,60 +9,39 @@ insert into cliente values (null,'Gato Tinto','SJM, IDAT, PLOP','qolca@gmail.com
 
 insert into cliente values (null,'Loco tinto','SJL, UDEMY, PLOP','udemy@gmail.com','Alexys','986251856');
 
+insert into cliente values (null,'Gato Naranja','SJM, IDAT, PLOP','qolcaaa@gmail.com','Alexander','986251358');
+
 insert into categoria values (null, 'categoria01');
 insert into categoria values (null, 'categoria02');
 insert into categoria values (null, 'categoria03');
-
-insert into servicio values (null, 'Soporte Técnico', 1);
-insert into servicio values (null, 'Cambio de componentes', 1);
-insert into servicio values (null, 'servicio03', 2);
-insert into servicio values (null, 'Soporte Técnico', 3);
-
-insert into empleado values (null, 'Rojas Caratallu', 'descripcion01', '74542565','rojas@hotmail.com','pepe lucas','986214547');
-insert into empleado values (null, 'Julca caser', 'descripcion02', '74542545','julca@hotmail.com','layton ezekiel','986214545');
-insert into empleado values (null, 'Varia Norton', 'descripcion03', '78542545','varia@hotmail.com','julia maria','986215603');
-=======
 insert into categoria values (null,'Hardware');
 insert into categoria values (null,'Software');
 
-insert into servicio values (null,'Mantenimiento',1);
-insert into servicio values (null,'Instalación de Windows',2);
-insert into servicio values (null,'Instalación de Linux',2);
-insert into servicio values (null,'Instalación de Sistemas Operativos',2);
-insert into servicio values (null,'Recuperación de datos',2);
-insert into servicio values (null,'Reparación',1);
-insert into servicio values (null,'Armado de computadoras',1);
-insert into servicio values (null, 'Soporte Técnico', 1);
-insert into servicio values (null, 'Cambio de componentes', 1);
->>>>>>> bb78ca506a36d9d643093bddd37d0079e35fd489
+insert into servicio values (null,'Mantenimiento',2);
+insert into servicio values (null,'Instalación de Windows',5);
+insert into servicio values (null,'Instalación de Linux',5);
+insert into servicio values (null,'Instalación de Sistemas Operativos',5);
+insert into servicio values (null,'Recuperación de datos',5);
+insert into servicio values (null,'Reparación',4);
+insert into servicio values (null,'Armado de computadoras',4);
+insert into servicio values (null, 'Soporte Técnico', 4);
+insert into servicio values (null, 'Cambio de componentes', 4);
 
 insert into rol values (null,'USER');
 insert into rol values (null,'ADMIN');
 insert into rol values (null,'CLIENTE');
 
-insert into cliente values (null,'Ortiz Diaz','SJM, IDAT, PLOP','natalyortiz.401@gmail.con','Melissa Nataly',923456123,852147369);
-insert into cliente values (null,'Torres Rosas','SJM, IDAT, PLOP','lalalalalal@gmail.con','Juan',923456555,852235369);
-insert into cliente values (null,'Vilco Ramos','SJM, IDAT, PLOP','pinomuerto@gmail.com','Jorge',945456123,986147369);
-insert into cliente values (null,'Gato Tinto','SJM, IDAT, PLOP','qolca@gmail.com','Alexander',986251357,948556325);
-insert into cliente values (null,'Loco tinto','SJL, UDEMY, PLOP','udemy@gmail.com','Alexys',986251856,948556312);
-insert into cliente values (null,'Gato Naranja','SJM, IDAT, PLOP','qolcaaa@gmail.com','Alexander',986251358,945556325);
-
-insert into empleado values (null, 'Rojas Caratallu', 'descripcion01', 74542565,'rojas@hotmail.com','true','pepe lucas',986214547);
-insert into empleado values (null, 'Julca caser', 'descripcion02', 74542545,'julca@hotmail.com','true','layton ezekiel',986214545);
-insert into empleado values (null, 'Varia Norton', 'descripcion03', 78542545,'varia@hotmail.com','true','julia maria',986215603);
-insert into empleado values (null,'Romes Rosas','lalalaalalalala',71220969,'JuanRomes@gmail.com','true','Juan Carlos',923556258);
-insert into empleado values (null,'Palmero Aguero','lalalaalalalala',71550858,'AlfonsoAguero@gmail.com','true','Alfonso',945985655);
-insert into empleado values (null,'Vigo Del Valle','lalalaalalalala',75615422,'Santiago.DelValle@gmail.com','true','Santiago',978885451);
-insert into empleado values (null,'Briones Ayala','lalalaalalalala',75569232,'ayalaFelix@gmail.com','false','Felix',956225336);
-insert into empleado values (null,'Varas Andreu','lalalaalalalala',75112145,'sofiaandreu@gmail.com','false','Sofia',911659449);
+insert into empleado values (null, 'Rojas Caratallu', 'descripcion01', '74542565','rojas@hotmail.com','true','pepe lucas','986214547');
+insert into empleado values (null, 'Julca caser', 'descripcion02', '74542545','julca@hotmail.com','true','layton ezekiel','986214545');
+insert into empleado values (null, 'Varia Norton', 'descripcion03', '78542545','varia@hotmail.com','true','julia maria','986215603');
+insert into empleado values (null,'Romes Rosas','lalalaalalalala','71220969','JuanRomes@gmail.com','true','Juan Carlos','923556258');
+insert into empleado values (null,'Palmero Aguero','lalalaalalalala','71550858','AlfonsoAguero@gmail.com','true','Alfonso','945985655');
+insert into empleado values (null,'Vigo Del Valle','lalalaalalalala','75615422','Santiago.DelValle@gmail.com','true','Santiago','978885451');
+insert into empleado values (null,'Briones Ayala','lalalaalalalala','75569232','ayalaFelix@gmail.com','false','Felix','956225336');
+insert into empleado values (null,'Varas Andreu','lalalaalalalala','75112145','sofiaandreu@gmail.com','false','Sofia','911659449');
 
 insert into administrador values (null, 'Paul Lorem', 'Rojas Castillo');
 insert into administrador values (null, 'Lorenzo Deam', 'Ambrose Leiya');
 
-<<<<<<< HEAD
-insert into usuario values (null,'admin001', 'Admin01', 1, null,null,2);
-=======
-insert into usuario values (null,'admin01', 'admin01', 1, null,null,2);
 
-
->>>>>>> bb78ca506a36d9d643093bddd37d0079e35fd489
+insert into usuario values (null,'admin001', 'admin01', 1, null,null,2);
